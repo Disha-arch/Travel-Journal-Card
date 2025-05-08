2934e9e128f11daf8afd37790d2f7c2b2176647a
Disha-arch <guptadisha.1009@gmail.com>
2025-05-08 20:52:36 +0530

    Add files via upload

---
+++ Data.jsx
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    mainImg: "mount fuji.jpg",
+    stats: {
+      locationImg: "loaction.png",
+      location: "J A P A N",
+      link: "https://g.co/kgs/sNJbdE4",
+    },
+    titleText: "Mount Fuji",
+    date: "12 Jan,2021 - 24 Jan,2021",
+    para: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in japan, for both Japenese and foreign tourists.",
+  },
+  {
+    id: 2,
+    mainImg: "sydney opera house.jpg",
+    stats: {
+      locationImg: "loaction.png",
+      location: "A U S T R A L I A",
+      link: "https://g.co/kgs/jeczcKo",
+    },
+    titleText: "Sydney Opera House",
+    date: "27 May, 2021 - 8 Jun, 2021",
+    para: "The Sydney Opera House is one of the most iconic landmarks in Australia and around the world. Located in Sydney, New South Wales, on Bennelong Point, it is famous for its unique sail-like design.",
+  },
+  {
+    id: 3,
+    mainImg: "Taj Mahal.jpg",
+    stats: {
+      locationImg: "loaction.png",
+      location: "I N D I A",
+      link: "https://www.google.co.in/maps/about/behind-the-scenes/streetview/treks/taj-mahal/",
+    },
+    titleText: "Taj Mahal",
+    date: "12 Sep, 1765 - 8 Jan, 1982",
+    para: "The Taj Mahal is one of the most famous and breathtaking monuments in the world, located in Agra, India. It is widely regarded as a symbol of love, built by the Mughal emperor Shah Jahan in memory of his beloved wife, Mumtaz Mahal, who passed away during childbirth in 1631.",
+  },
+];
